cd698a4ca5a3063f12319e3005077fb477789588
Ruan Lucas <ruan.lucss@gmail.com>
2024-07-14 15:37:53 -0300

    refactor: Update routes for log processing and report generation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LogFileController;
+use App\Http\Controllers\PanelController;
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PanelController::class, 'index'])->name('welcome');
+Route::get('/log-files', [LogFileController::class, 'index'])->name('logReader.files');
+Route::post('/log-files', [LogFileController::class, 'upload'])->name('logReader.uploadLogFile');
+Route::get('/generate-report', [ReportController::class, 'generateReport'])->name('report.generateReport');
