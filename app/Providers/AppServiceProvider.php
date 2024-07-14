f434d53bfe7c1430bcfb86b6b0c43b64c7ee3274
Ruan Lucas <ruan.lucss@gmail.com>
2024-07-14 15:32:31 -0300

    feat: Bind repositories interfaces to their repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\ConsumerRepositoryInterface;
+use App\Repositories\Eloquent\ConsumerRepository;
+use App\Repositories\Eloquent\LogFileProcessRepository;
+use App\Repositories\Eloquent\RequestRepository;
+use App\Repositories\Eloquent\ServiceRepository;
+use App\Repositories\LogFileProcessRepositoryInterface;
+use App\Repositories\RequestRepositoryInterface;
+use App\Repositories\ServiceRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(ServiceRepositoryInterface::class, ServiceRepository::class);
+        $this->app->bind(ConsumerRepositoryInterface::class, ConsumerRepository::class);
+        $this->app->bind(RequestRepositoryInterface::class, RequestRepository::class);
+        $this->app->bind(LogFileProcessRepositoryInterface::class, LogFileProcessRepository::class);
     }
 
     /**
